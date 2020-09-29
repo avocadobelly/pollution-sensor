9f037c128250ed4b7796b7347e168a715cceba1b
gconnellbass <georgia.connell-bass@altran.com>
2020-09-29 16:58:20 +0100

    Formatted locations data. Created aws queue. Granted permission for sns topic to write to the sqs queue.Subscribed to the queue

--- pollution-app/pollution_app/__init__.py
+++ pollution-app/pollution_app/__init__.py
@@ -1,12 +1,46 @@
 __version__ = '0.1.0'
-
+import json
 import boto3
-from .aws_creds import s3_bucket
-
 
 def main():
     s3 = boto3.client('s3')
-    s3.download_file(s3_bucket, 'locations.json', 'locations.json')
+    sqs = boto3.client('sqs')
+    sns = boto3.client('sns')
+    f = s3.get_object(Bucket='eventprocessing-altran-locationss3bucket-1ub1fsm0jlky7', Key='locations.json')['Body'].read().decode('utf-8')
 
+    locations = json.loads(f)
+
+    for location in locations:
+        print("{} x={} y={}".format(location['id'], location['x'], location['y']))
+
+    aws_queue = sqs.create_queue(QueueName='queue')
+    queue_url = aws_queue['QueueUrl']
+    arn = sqs.get_queue_attributes(QueueUrl=queue_url, AttributeNames=['QueueArn'])
+    queue_arn = arn['Attributes']['QueueArn']
+    print(queue_arn)
+
+    # grant permission for sns topic to write to the sqs queue
+    topic_arn = 'arn:aws:sns:eu-west-1:552908040772:EventProcessing-Altran-snsTopicSensorDataPart1-1HJ83JI0COKVB'
+
+    policy_document = {
+        'Version': '2012-10-17',
+        'Statement': [{
+            'Sid': f'allow-subscription-{topic_arn}',
+            'Effect': 'Allow',
+            'Principal': {'AWS': '*'},
+            'Action': 'SQS:SendMessage',
+            'Resource': f'{queue_arn}',
+            'Condition': {
+                'ArnEquals': {'aws:SourceArn': f'{topic_arn}'}
+            }
+        }]
+    }
+    policy_json = json.dumps(policy_document)
+
+    sqs.set_queue_attributes(QueueUrl=queue_url, Attributes={'Policy': policy_json})
+
+    # subscribe event source to the queue
+    subscription_to_queue = sns.subscribe(TopicArn=topic_arn, Protocol='sqs', Endpoint=queue_arn)
+    print(subscription_to_queue)
 
 main()
